488251fffc889ab3d1b6dd3c97b1a4f800adff12
KaoChristian <kao.c@hotmail.fr>
2024-04-10 15:21:38 +0200

    add validator

--- myapp/app/users/controller.js
+++ myapp/app/users/controller.js
@@ -3,10 +3,15 @@ const UsersService = require('./service')
 const usersService = new UsersService();
 
 function createUser(req, res) {
-    const response = usersService.createUsers(req.body);
-    
+    // const paramsValidation = [
+    //     body('fullName').withMessage('Full name is required.'),
+    //     body('job').withMessage('Job is required.'),
+    //     body('role').withMessage('Role is required.'),
+    //     body('email').withMessage('Email is required.'),
+    //     body('password').withMessage('Password is required.'),
+    // ]
     return res.send(response);
-}
+};
 
 module.exports = {
     createUser,
